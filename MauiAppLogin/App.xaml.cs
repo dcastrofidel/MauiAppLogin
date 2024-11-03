607083fa8a08b89fadc1b0479e388416e5ac6a62
dcastrofidel <dgarcia.fidel@gmail.com>
2024-11-03 18:50:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppLogin/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiAppLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            string? usuario_logado = null;
+
+            Task.Run(async () =>
+            {
+
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+
+                if (usuario_logado == null)
+                {
+                    MainPage = new Login();
+
+                }
+                else
+                { 
+                    MainPage = new Protegida();
+                
+                
+                }
+                
+            });
+
+            MainPage = new Login();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 400;
+
+            return window;
+        }
+    }
+
+}
